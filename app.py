f0fafebd1e29b1e81e6edbafd2c6ea4b472f6104
ajlbg <120402175+ajlbg@users.noreply.github.com>
2023-08-25 10:59:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-  from flask import Flask, render_template
-  import random
+from flask import Flask, render_template
+import random
 
-  app = Flask(__name__)
+app = Flask(__name__)
 
-  # list of gif images - To-Be-Updated
-  images = []
+# list of gif images - To-Be-Updated
+images = []
 
-  @app.route('/')
-  def index():
-      url = random.choice(images)
-      return render_template('index.html', url=url)
+@app.route('/')
+def index():
+    url = random.choice(images)
+    return render_template('index.html', url=url)
 
-  if __name__ == "__main__":
-      app.run(host="0.0.0.0")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
