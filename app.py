829f637bb0c7ab8cae74673912aad67f734b2e25
ajlbg <120402175+ajlbg@users.noreply.github.com>
2023-08-25 10:21:39 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+  from flask import Flask, render_template
+  import random
+
+  app = Flask(__name__)
+
+  # list of gif images - To-Be-Updated
+  images = []
+
+  @app.route('/')
+  def index():
+      url = random.choice(images)
+      return render_template('index.html', url=url)
+
+  if __name__ == "__main__":
+      app.run(host="0.0.0.0")
